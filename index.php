d838996c17dc375d2336c68c07ad6d4723535195
PhongVu <vanphongltktv@gmail.com>
2021-04-07 23:42:17 +0700

    backend in file connDB

--- index.php
+++ index.php
@@ -28,93 +28,50 @@
 </div>
 <div class="col-sm-5 col-md-4">
 <h3>Reservation</h3>
-    <form role="form" class="wowload fadeInRight">
+    <form method="post" role="form" class="wowload fadeInRight" action="connDB.php">
         <div class="form-group">
-            <input type="text" class="form-control"  placeholder="Name">
+            <input type="text" class="form-control"  placeholder="Name" name="name">
         </div>
         <div class="form-group">
-            <input type="email" class="form-control"  placeholder="Email">
+            <input type="email" class="form-control"  placeholder="Email" name="email">
         </div>
         <div class="form-group">
-            <input type="Phone" class="form-control"  placeholder="Phone">
+            <input type="Phone" class="form-control"  placeholder="Phone" name="phone">
         </div>        
-        <div class="form-group">
+        <!-- <div class="form-group">
             <div class="row">
-            <div class="col-xs-6">
-            <select class="form-control">
-              <option>No. of Rooms</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-            </div>        
-            <div class="col-xs-6">
-            <select class="form-control">
+                 
+            <select class="form-control" name="accomodates">
               <option>No. of Adult</option>
+              <option>1</option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
             </select>
-            </div></div>
+            </div>
+        </div>
+         -->
+        <div class="form-group">
+            <div class="form-control">
+                <!-- <label for="checkin">Check In:</label> -->
+                <input type="date" id="checkin" name="checkin">
+            </div>
         </div>
         <div class="form-group">
-            <div class="row">
-            <div class="col-xs-4">
-              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                <option>Date</option>
-                <option value="01">1</option>
-                <option value="02">2</option>
-                <option value="03">Mar (03)</option>
-                <option value="04">Apr (04)</option>
-                <option value="05">May (05)</option>
-                <option value="06">June (06)</option>
-                <option value="07">July (07)</option>
-                <option value="08">Aug (08)</option>
-                <option value="09">Sep (09)</option>
-                <option value="10">Oct (10)</option>
-                <option value="11">Nov (11)</option>
-                <option value="12">Dec (12)</option>
-              </select>
+            <div class="form-control">
+                <!-- <label for="checkout">Check Out:</label> -->
+                <input type="date" id="checkout" name="checkout">
             </div>
-            <div class="col-xs-4">
-              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                <option>Month</option>
-                <option value="01">Jan (01)</option>
-                <option value="02">Feb (02)</option>
-                <option value="03">Mar (03)</option>
-                <option value="04">Apr (04)</option>
-                <option value="05">May (05)</option>
-                <option value="06">June (06)</option>
-                <option value="07">July (07)</option>
-                <option value="08">Aug (08)</option>
-                <option value="09">Sep (09)</option>
-                <option value="10">Oct (10)</option>
-                <option value="11">Nov (11)</option>
-                <option value="12">Dec (12)</option>
-              </select>
-            </div>
-            <div class="col-xs-4">
-              <select class="form-control" name="expiry-year">
-                <option value="13">2013</option>
-                <option value="14">2014</option>
-                <option value="15">2015</option>
-                <option value="16">2016</option>
-                <option value="17">2017</option>
-                <option value="18">2018</option>
-                <option value="19">2019</option>
-                <option value="20">2020</option>
-                <option value="21">2021</option>
-                <option value="22">2022</option>
-                <option value="23">2023</option>
-              </select>
-            </div>
-          </div>
         </div>
-        <div class="form-group">
-            <textarea class="form-control"  placeholder="Message" rows="4"></textarea>
-        </div>
+        <!-- <div class="form-group">
+            <textarea class="form-control"  placeholder="Message" rows="4" name="message"></textarea>
+        </div> -->
         <button class="btn btn-default">Submit</button>
     </form>    
 </div>
---
+++ validateForm.php
@@ -0,0 +1,87 @@
+<?php
+$nameErr = $emailErr = $phoneErr = $checkinErr = $checkoutErr = $accomodatesErr = "";
+$name = $email = $phone = $checkin = $checkout = "";
+$accomodates = 0;
+$checkErr = false;
+$report = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+    $checkErr = true;
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+    $checkErr = true;
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+      $checkErr = true;
+    }
+  }
+
+  if (empty($_POST["phone"])) {
+    $phoneErr = "Phone is required";
+    $checkErr = true;
+  } else {
+    $phone = test_input($_POST["phone"]);
+    $pattern = '/((09|03|07|08|05)+([0-9]{8})\b)/';
+    if (preg_match($pattern, $phone, $match) == 0) {
+      $phoneErr = "Invalid phone number";
+      $checkErr = true;
+    }
+  }
+  if (empty($_POST["accomodates"]) || $_POST["accomodates"] == "No. of Adult") {
+    $accomodatesErr = "Accomodates is required";
+    $checkErr = true;
+  } else {
+    $accomodates = test_input($_POST["accomodates"]);
+  }
+  if (empty($_POST["checkin"])) {
+    $checkinErr = "Check-in date is required";
+    $checkErr = true;
+  } else {
+    $checkin = test_input($_POST["checkin"]);
+  }
+  if (empty($_POST["checkout"])) {
+    $checkoutErr = "Check-out date is required";
+    $checkErr = true;
+  } else {
+    $checkout = test_input($_POST["checkout"]);
+  }
+  if ($checkErr == true) {
+    if ($nameErr != "" ) {
+      $report .= $nameErr . "<br>";
+    }
+    if ($emailErr != "" ) {
+      $report .= $emailErr . "<br>";
+    }
+    if ($phoneErr != "" ) {
+      $report .= $phoneErr . "<br>";
+    }
+    if ($accomodatesErr != "" ) {
+      $report .= $accomodatesErr . "<br>";
+    }
+    if ($checkinErr != "" ) {
+      $report .= $checkinErr . "<br>";
+    }
+    if ($checkoutErr != "" ) {
+      $report .= $checkoutErr . "<br>";
+    }
+    $report = '<span style=\'color:#FF0400\'>'.$report.'</span>';
+    echo $report;
+    echo '<a href="index.php">Go back</a>';
+  }
+
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>

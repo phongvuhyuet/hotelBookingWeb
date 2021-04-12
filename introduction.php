84e27cfc5c450b2b41d93dcc43fc8c4c767716be
hoikhongtuyen <=>
2021-04-13 00:50:38 +0700

    mssv

--- introduction.php
+++ introduction.php
@@ -6,11 +6,11 @@
             style="line-height:1.2em;"><span class="color_19">Who</span></span></h6>
     <h5 style="text-align:center;font-family: Brush Script MT; font-size: 30px; " class="font_5">We Are</h5>
     <hr height: 10px; width="30%" />
-    <h5 style="text-align:center;"><br><br><br>19020268 - <a href="https://www.facebook.com/phongltktv">Vũ Văn
+    <h5 style="text-align:center;"><br><br><br>19020392 - <a href="https://www.facebook.com/phongltktv">Vũ Văn
             Phong</a> <br><br></h5>
     <h5 style="text-align:center;">19020268 - <a href="https://www.facebook.com/phuongduy.tran.9887/">Trần Phương
             Duy</a> <br><br></h5>
-    <h5 style="text-align:center;">19020268 - <a href="https://www.facebook.com/profile.php?id=100008400150029">Đỗ Văn
+    <h5 style="text-align:center;">19020453 - <a href="https://www.facebook.com/profile.php?id=100008400150029">Đỗ Văn
             Thức</a> <br><br></h5>
 
 
